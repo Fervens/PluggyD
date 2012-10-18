82102d662caccd56988b1361d68f4bb1d3c947db
Ryan Millikin <rmillikin@gmail.com>
2012-10-18 18:10:19 -0500

    Message passing no longer breaks if you haven't opened the settings page. Moved lots of stuff from the settings page to the background page.

--- js/background.js
+++ js/background.js
@@ -1,10 +1,6 @@
+var pluggyd = {};
+
 (function() {
-	chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-		if (tab.url.indexOf('plug.dj') > -1) {
-			chrome.pageAction.show(tabId);
-		}
-	});
-	
 	window.setTimeout(function () {
 		var _gaq = _gaq || [];
 		_gaq.push(['_setAccount', 'UA-35666244-1']);
@@ -18,4 +14,47 @@
 		s = document.getElementsByTagName('script')[0];
 		s.parentNode.insertBefore(ga, s);
 	}, 10000);
+	
+	var defaultPreferences = {
+		slimModeEnabled: false //If true, a skinnier version of plug.dj rooms will be used.
+	};
+	
+	pluggyd.loadPreferences = function() {
+		if (!window.localStorage.pluggydPreferences) {
+			return $.extend({}, defaultPreferences);
+		}
+		
+		return $.extend({}, JSON.parse(window.localStorage.pluggydPreferences), defaultPreferences);
+	};
+	
+	pluggyd.savePreferences = function(preferences) {
+        window.localStorage.pluggydPreferences = JSON.stringify(preferences);
+	};
+	
+	pluggyd.sendMessageToTabs = function($message) {
+		chrome.tabs.query({url: "http://*.plug.dj/*"}, function(tabs) {
+			for (var i = 0; i < tabs.length; i++) {
+				chrome.tabs.sendMessage(tabs[i].id, $message);
+			}
+		});
+		
+		chrome.tabs.query({url: "http://plug.dj/*"}, function(tabs) {
+			for (var i = 0; i < tabs.length; i++) {
+				chrome.tabs.sendMessage(tabs[i].id, $message);
+			}
+		});
+	};
+	
+	pluggyd.preferences = pluggyd.loadPreferences();
+	chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+		if (tab.url.indexOf('plug.dj') > -1) {
+			chrome.pageAction.show(tabId);
+		}
+	});
+	
+	chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
+		if (message == 'getPreferences') {
+			sendResponse(pluggyd.preferences);
+		}
+	});
 })();
--- js/options.js
+++ js/options.js
@@ -1,62 +1,22 @@
 (function($) {
-	var preferences = {};
-	var defaultPreferences = {
-		slimModeEnabled: false
-	};
+	var pluggyd = chrome.extension.getBackgroundPage().pluggyd;
 	
 	$(function() {
-		loadPreferences();
-		setupMessageHandlers();
 		setupPage();
 	});
-	
-	function setupMessageHandlers() {
-		chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
-			if (message == 'getPreferences') {
-				sendResponse(preferences);
-			}
-		});
-	}
 	
 	function setupPage() {
 		$('#pluginVersion').text(chrome.app.getDetails().version);
 		
 		var $slimModeEnabled = $('#slimModeEnabled');
-		$slimModeEnabled.prop('checked', preferences.slimModeEnabled);
+		$slimModeEnabled.prop('checked', pluggyd.preferences.slimModeEnabled);
 		$slimModeEnabled.click(function() {
-			preferences.slimModeEnabled = $slimModeEnabled.is(':checked');
-			savePreferences(preferences);
-			sendMessageToTabs({
+			pluggyd.preferences.slimModeEnabled = $slimModeEnabled.is(':checked');
+			pluggyd.savePreferences(pluggyd.preferences);
+			pluggyd.sendMessageToTabs({
 				'event': 'slimModeChanged',
-				'args': preferences.slimModeEnabled
+				'args': pluggyd.preferences.slimModeEnabled
 			});
-		});
-	}
-	
-	function loadPreferences() {
-		if (!window.localStorage.pluggydPreferences) {
-			preferences = defaultPreferences;
-			return;
-		}
-		
-		preferences = JSON.parse(window.localStorage.pluggydPreferences);
-	}
-	
-	function savePreferences(preferences) {
-        window.localStorage.pluggydPreferences = JSON.stringify(preferences);
-	}
-	
-	function sendMessageToTabs($message) {
-		chrome.tabs.query({url: "http://*.plug.dj/*"}, function(tabs) {
-			for (var i = 0; i < tabs.length; i++) {
-				chrome.tabs.sendMessage(tabs[i].id, $message);
-			}
-		});
-		
-		chrome.tabs.query({url: "http://plug.dj/*"}, function(tabs) {
-			for (var i = 0; i < tabs.length; i++) {
-				chrome.tabs.sendMessage(tabs[i].id, $message);
-			}
 		});
 	}
 })(jQuery);
