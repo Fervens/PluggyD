57bc86572d2e2c257702f96f51b641823f48ece1
Ryan Millikin <rmillikin@gmail.com>
2012-10-17 22:14:13 -0500

    Added analytics, hooked up the option with local storage

--- js/background.js
+++ js/background.js
@@ -4,4 +4,18 @@
 			chrome.pageAction.show(tabId);
 		}
 	});
+	
+	window.setTimeout(function () {
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-35666244-1']);
+		_gaq.push(['_trackPageview']);
+		_gaq.push(['_trackEvent', 'Version', chrome.app.getDetails().version]);
+
+		var ga = document.createElement('script'), s;
+		ga.type = 'text/javascript';
+		ga.async = true;
+		ga.src = 'https://ssl.google-analytics.com/ga.js';
+		s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(ga, s);
+	}, 10000);
 })();
--- js/options.js
+++ js/options.js
@@ -1,5 +1,13 @@
 (function($) {
 	$(function() {
 		$('#pluginVersion').text(chrome.app.getDetails().version);
+		
+		var $slimModeEnabled = $('#slimModeEnabled');
+		var slimModeEnabled = (window.localStorage.slimModeEnabled == "checked");
+		$slimModeEnabled.prop('checked', slimModeEnabled);
+		$slimModeEnabled.click(function() {
+			window.localStorage.slimModeEnabled = $slimModeEnabled.is(':checked') ? "checked" : "";
+			console.log(window.localStorage.slimModeEnabled);
+		});
 	});
 })(jQuery);
